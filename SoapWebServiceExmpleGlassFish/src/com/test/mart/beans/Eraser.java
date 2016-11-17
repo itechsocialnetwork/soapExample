59814eb318ea69e4ed640ddb0433210342e87a2a
itechsocialnetwork <vedant.itechsoftware@gmail.com>
2016-11-18 01:59:05 +0530

    Eraser.java for eraser

---
+++ SoapWebServiceExmpleGlassFish/src/com/test/mart/beans/Eraser.java
@@ -0,0 +1,22 @@
+package com.test.mart.beans;
+
+public class Eraser {
+	
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
